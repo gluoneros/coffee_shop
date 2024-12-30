d6e43b94175940dc24e481c042ef4080802fb154
gluoneros <gluoneros@gmail.com>
2024-12-30 00:06:39 -0500

    tailwind

--- coffee_shop/urls.py
+++ coffee_shop/urls.py
@@ -25,5 +25,5 @@ urlpatterns = [
     path("products/", include("products.urls")),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#if settings.DEBUG:
+#    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
