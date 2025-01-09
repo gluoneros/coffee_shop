d1bc71bd7e56b269bdd3e384d06bf885c653e0d1
gluoneros <gluoneros@gmail.com>
2025-01-08 19:11:45 -0500

    header

--- coffee_shop/settings.py
+++ coffee_shop/settings.py
@@ -107,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-co'
 
 TIME_ZONE = 'UTC'
 
@@ -141,3 +141,4 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = "tailwind"
 CRISPY_TEMPLATE_PACK = "tailwind"
 
 LOGIN_REDIRECT_URL = 'list_product'
+LOGOUT_REDIRECT_URL = 'list_product'
